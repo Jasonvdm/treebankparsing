8f39b913b7ad68f8f9bcf7617661c26f4d7b85f8
Jason <jason.merwe@gmail.com>
2014-10-21 18:54:31 -0700

    asda

--- java/src/cs224n/assignment/PCFGParser.java
+++ java/src/cs224n/assignment/PCFGParser.java
@@ -3,7 +3,7 @@ package cs224n.assignment;
 import cs224n.ling.Tree;
 import java.util.*;
 import cs224n.util.PriorityQueue;
-import cs224n.util.Pair;
+import cs224n.util.Triplet;
 
 
 /**
@@ -13,6 +13,7 @@ public class PCFGParser implements Parser {
     private Grammar grammar;
     private Lexicon lexicon;
     private HashMap<String, Integer> nontermIndices = new HashMap<String, Integer>();
+    Set<String>nonterms;
 
     public void train(List<Tree<String>> trainTrees) {
         for (int i=0;i < trainTrees.size(); i++) {
@@ -25,38 +26,76 @@ public class PCFGParser implements Parser {
     }
 
     public Tree<String> getBestParse(List<String> sentence) {
-        Set<String>nonterms = getAllNonTerms();
+        nonterms = getAllNonTerms();
         double[][][] score = new double[sentence.size() + 1][sentence.size() + 1][nonterms.size()];
-        Pair back[][][] = new Pair[sentence.size() + 1][sentence.size() + 1][nonterms.size()];
+        Triplet back[][][] = new Triplet[sentence.size() + 1][sentence.size() + 1][nonterms.size()];
 
         for (int i=0; i < sentence.size(); i++){
             for (String a:nonterms){
-                score[i][i+1][nontermIndices.get(a)] = scoreTagging(sentence.get(i),a);
+                score[i][i+1][nontermIndices.get(a)] = lexicon.scoreTagging(sentence.get(i),a);
+                back[i][i+1][nontermIndices.get(a)] = new Triplet(-2,a, sentence.get(i));
             }
+            addUnaryRule(score, back, i, i+1);
+        }
 
-            boolean added = true;
+        for (int span=2; span<sentence.size(); span++){
+            for (int begin=0; begin < sentence.size() - span; begin++){
+                int end = begin + span;
+
+                for (int split = begin+1; split < end-1; split++){
+                    for (String nterm:nonterms){
+                        List<Grammar.BinaryRule> masterRules = grammar.getBinaryRulesByLeftChild(nterm);
+                        masterRules.addAll(grammar.getBinaryRulesByRightChild(nterm)); 
+
+                        for (Grammar.BinaryRule rule : masterRules){
+                            String a = rule.getParent();
+                            String b = rule.getLeftChild();
+                            String c = rule.getRightChild();
+
+                            int indexA = nontermIndices.get(a);
+                            int indexB = nontermIndices.get(b);
+                            int indexC = nontermIndices.get(c);
+
+                            double prob = score[begin][split][indexB] * score[split][end][indexC] * rule.getScore();
+                            if (prob > score[begin][end][indexA]) {
+                                score[begin][end][indexA] = prob;
+                                back[begin][end][indexA] = new Triplet(split, b, c);
+                            }
+                        }
+                    }
+                    addUnaryRule(score, back, begin, end);
+                }
+            }
+        }
+
+
+        return null;
+    }
+
+    private void addUnaryRule(double[][][] score, Triplet back[][][], int begin, int end) 
+    {
+        boolean added = true;
 
             while(added) {
                 added = false;
                 for (String b:nonterms){
-                    for (Grammar.UnaryRule rule : List<UnaryRule> Grammar.getUnaryRulesByChild(a)) {
+                    for (Grammar.UnaryRule rule : grammar.getUnaryRulesByChild(b)) {
                         String a = rule.getParent();
 
                         int indexA = nontermIndices.get(a);
                         int indexB = nontermIndices.get(b);
 
-                        if (score[i][i+1][indexB] > 0){
+                        if (score[begin][end][indexB] > 0){
                             double prob = rule.getScore();
-                            if (prob > score[i][i+1][indexA]) {
-                                score[i][i+1][indexA] = prob;
-                                back[i][i+1][indexA] = b;
+                            if (prob > score[begin][end][indexA]) {
+                                score[begin][end][indexA] = prob;
+                                back[begin][end][indexA] = new Triplet(-1, a, b);
+                                added = true;
                             }
                         }
                     } 
                 }
             }
-        }
-        return null;
     }
 
     private Set<String> getAllNonTerms(){
@@ -95,7 +134,7 @@ public class PCFGParser implements Parser {
         int index = -1;
         for(String a: nonterms){
             index++;
-            nontermIndices[a] = index;
+            nontermIndices.put(a, index);
         }
         return nonterms;
     }
